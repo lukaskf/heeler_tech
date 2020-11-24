aa0b699933059a34750c9040e2365cf70eb77d3d
Lukas K-F <lukaskf@gmail.com>
2020-11-23 21:29:03 -0600

    Data processing for works order in ScheduleController   - helper method defined to generate array of hashes with     info for each workorder entry in UI

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,14 +1,20 @@
 class ScheduleController < ApplicationController
+    helper_method :get_workorder_entries
     def index
         @technicians = Technician.all
-        @locations = Location.all
-        @workorders = Workorder.all
 
         @todays_date = "10/1/19"
-
-        def find_workorder(i)
-            current_time = Time.parse("#{todays_date} #{i}:00")
-            workorder = technician.workorders.find_by time: "#{@todays_date} "
+    end
+    def get_workorder_entries(technician)
+        workorders = technician.workorders.where(date: @todays_date)
+        workorder_entries = []
+        workorders.each do |workorder|
+            entry = {}
+            entry["size"] = (workorder.duration/60).ceil()
+            entry["position"] = workorder.time.to_s
+            entry["info"] = "Location: #{workorder.location.name}, City: #{workorder.location.city}, Start-Time: #{workorder.time}, Price: #{workorder.price}"
+            puts entry
+            workorder_entries.append(entry)
         end
     end
 end
