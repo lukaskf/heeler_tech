c5db7dc258ea03cdc3e9c05369aa3f92bcd1ed59
Lukas K-F <lukaskf@gmail.com>
2020-11-24 22:31:34 -0600

    Refactor Workorder Rendering, Holders rendered, onclick function   - Workorders rendering refactored to be in technician dependent loop   - Spaces inbetween workorders rendered as holders   - onclick function created for holders

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,21 +1,23 @@
 class ScheduleController < ApplicationController
-    helper_method :get_workorder_entries
+    helper_method :start_time
+    helper_method :end_time
     def index
         @technicians = Technician.all
         @workorders = Workorder.all
 
         @todays_date = DateTime.parse('20191001 6: 00: 00')
     end
-    def get_workorder_entries()
-        workorders = Workorders.all.where(date: @todays_date)
-        @workorder_entries = []
-        workorders.each do |workorder|
-            entry["tech"] = "tech-#{workorder.technician_id}"
-            entry["duration"] = (workorder.duration/60).ceil()
-            entry["start_time"] = workorder.time.to_s
-            entry["info"] = "Location: #{workorder.location.name}, City: #{workorder.location.city}, Start-Time: #{workorder.time}, Price: #{workorder.price}"
-            puts entry
-            workorder_entries.append(entry)
+    def get_holder_entries(occupied_times)
+        holders = []
+        occupied_times.each_with_index do |block, index|
+            if index==0 && block[:start_time]!=6 #first workorder doesn't start at first slot
+                holders.append({"start_time": 6, "end_time": block[:start_time]})
+            elsif index==occupied_times.length-1 && block[:end_time]!=18 #last workorder doesn't end at last slot
+                holders.append({"start_time": block[:end_time], "end_time": 18})
+            elsif index!=occupied_times.length-1 #put holder that starts at end of current workorder, and ends at the start of the next workorder
+                holders.append({"start_time": block[:end_time], "end_time": occupied_times[index+1][:start_time]})
+            end
         end
+        return occupied_times
     end
 end
--- app/models/workorder.rb
+++ app/models/workorder.rb
@@ -2,4 +2,11 @@ class Workorder < ApplicationRecord
     belongs_to :technician, class_name: "Technician"
     belongs_to :location, class_name: "Location"
     validates :time, uniqueness: {scope: [:technician_id, :location_id]}
+
+    def start_time
+        return self.time.strftime("%H").to_i
+    end 
+    def end_time
+       return self.start_time+(self.duration.to_f/60).ceil
+    end
 end
